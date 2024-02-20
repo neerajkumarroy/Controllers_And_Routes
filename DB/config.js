3a3d2d421934b5dd33841c8861b89a74935015e1
neerajkumarroy <neerajkumarroyss@gmail.com>
2024-02-20 22:07:26 +0530

    add comment

--- DB/config.js
+++ DB/config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
     mongoose.connect(process.env.MONGODB_URL, {
-        // useNewUrlParser: true,
+        // useNewUrlParser: true
         // useUnifiedTopology: true
     }).then(() => {
         console.log("DB connect successFully.....");
@@ -11,5 +11,4 @@ const connectDB = () => {
         console.log(error);
     });
 };
-
 module.exports = connectDB;
