10dd8dd375cca384920d1a760ba4d0cbdfe17451
neerajkumarroy <neerajkumarroyss@gmail.com>
2024-03-01 23:29:40 +0530

    add files

--- DB/config.js
+++ DB/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-    mongoose.connect(process.env.MONGODB_URL, {
+    mongoose.connect(process.env.DB_URL, {
         // useNewUrlParser: true
         // useUnifiedTopology: true
     }).then(() => {
